a7bc7e8f474a1ffc1bdbddffdb6c4c507f168a21
Lexinor <alternonolimit@gmail.com>
2023-10-26 21:41:52 +0200

    Création du fichier testing.yml, Modif de la trad FR, Modif de la config

--- shared/shared.lua
+++ shared/shared.lua
@@ -3,31 +3,31 @@ Shared.MaxFrequency = 500.00 -- Max Limit of Radio Channel
 Shared.RestrictedChannels = {
     [1] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police"}
     },
     [2] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police"}
     },
     [3] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police"}
     },
     [4] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police"}
     },
     [5] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police"}
     },
     [6] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police"}
     },
     [7] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police"}
     },
     [8] = { -- channel id
         type = 'job', -- job/gang
@@ -35,24 +35,24 @@ Shared.RestrictedChannels = {
     },
     [9] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"ambulance"}
     },
     [10] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"ambulance"}
     },
-    [420] = { -- channel id
-        type = 'gang', -- job/gang
-        name = {"ballas"}
-    },
-    [421] = { -- channel id
-        type = 'gang', -- job/gang
-        name = {"lostmc"}
-    },
-    [422] = {
-        type = 'gang', -- job/gang
-        name = {"vagos"}
-    },
+    -- [420] = { -- channel id
+    --     type = 'gang', -- job/gang
+    --     name = {"ballas"}
+    -- },
+    -- [421] = { -- channel id
+    --     type = 'gang', -- job/gang
+    --     name = {"lostmc"}
+    -- },
+    -- [422] = {
+    --     type = 'gang', -- job/gang
+    --     name = {"vagos"}
+    -- },
 }
 
 lib.locale()
